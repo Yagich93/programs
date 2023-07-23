49dce40fc40c2b9f6d24e0175a4224dccbc747d5
Iakov Lushin <yagich@bk.ru>
2023-07-23 14:11:39 +0400

    Add tracking available IDs

--- src/repositories/ProgramRepository.spec.ts
+++ src/repositories/ProgramRepository.spec.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata"
+import { faker } from "@faker-js/faker"
 
 import { ProgramRepository } from "./ProgramRepository"
 import { generateProgramData, generatePrograms } from "../../__test__/data"
@@ -25,9 +26,11 @@ describe("ProgramRepository", () => {
 
     describe("addProgram", () => {
         let programData: ProgramData
+        let nextId: number
 
         beforeEach(() => {
             programData = generateProgramData()
+            nextId = faker.number.int(100)
         })
 
         afterEach(() => {
@@ -46,6 +49,22 @@ describe("ProgramRepository", () => {
             const program = await programRepository.addProgram(programData)
 
             expect(program).toEqual(expect.objectContaining(programData))
+        })
+
+        it("should assign next available id", async () => {
+            programRepository["nextId"] = nextId
+
+            const program = await programRepository.addProgram(programData)
+
+            expect(program.id).toEqual(nextId)
+        })
+
+        it("should increment next available id", async () => {
+            programRepository["nextId"] = nextId
+
+            await programRepository.addProgram(programData)
+
+            expect(programRepository["nextId"]).toEqual(nextId + 1)
         })
     })
 })
--- src/repositories/ProgramRepository.ts
+++ src/repositories/ProgramRepository.ts
@@ -4,13 +4,14 @@ import { Program, ProgramData } from "../models"
 @injectable()
 export class ProgramRepository {
     private programs: Program[] = []
+    private nextId = 0
 
     async listPrograms(): Promise<Program[]> {
         return this.programs
     }
 
     async addProgram(programData: ProgramData): Promise<Program> {
-        const id = 0
+        const id = this.nextId++
         const program = { ...programData, id }
         this.programs.push(program)
         return program
